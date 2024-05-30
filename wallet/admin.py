f0227be06e3ba31c690af2742824bb9a87e3058a
Peyman Ezzati <pezzati@ce.sharif.edu>
2024-05-30 20:55:21 +0330

    Add Models to admin.py and Database Migrations

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from wallet.models import Asset, Category, Market, Wallet
+
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'symbol', 'price', 'quantity', 'total')
+    search_fields = ('name', 'symbol')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+
+@admin.register(Market)
+class MarketAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+    search_fields = ('name',)
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('asset', 'category', 'market', 'description')
+    search_fields = ('asset', 'category', 'market')
+    list_filter = ('asset', 'category', 'market')
